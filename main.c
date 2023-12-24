4a24b5f74f9e4827a4b62506cbf94ef17826f137
Aditi Khare <89248363+Aditikhare1101@users.noreply.github.com>
2023-12-24 18:43:49 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char  s[20],c[20];
+    int result=0;
+    printf("enetr s:");
+    gets(s);
+    printf("enter c:" );
+    gets(c);
+    printf("string copy is %d", result=strcmp(s,c));
+}
+
+
